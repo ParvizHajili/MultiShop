5165c7580c23f523b2bcaeb2529669485db4962e
Parviz <parviz.hajili@gmail.com>
2022-10-19 12:38:18 +0400

    10/19/2022 12:38

---
+++ WebUI/Areas/Admin/Controllers/UserRoleController.cs
@@ -0,0 +1,59 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Areas.Admin.ViewModels;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserRoleController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRoleController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(string id)
+        {
+            if (id == null)
+                return NotFound();
+
+            User user = await _userManager.FindByNameAsync(id);
+            if (user == null) return NotFound();
+            var userRoles = (await _userManager.GetRolesAsync(user)).ToList();
+            var roles = _roleManager.Roles.Select(role => role.Name).ToList();
+            AddUserRoleVm addUserRoleVm = new()
+            {
+                User = user,
+                Roles = roles.Except(userRoles)
+            };
+
+            return View(addUserRoleVm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(string id, string role)
+        {
+            if (id == null) return NotFound();
+
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+
+            var result = await _userManager.AddToRoleAsync(user, role);
+            if (result.Succeeded) return RedirectToAction("Index");
+
+            return View();
+        }
+    }
+}
---
+++ WebUI/Areas/Admin/ViewModels/AddUserRoleVm.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Concrete;
+
+namespace WebUI.Areas.Admin.ViewModels
+{
+    public class AddUserRoleVm
+    {
+        public User User { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
